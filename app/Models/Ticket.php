6e57abf043ba9635f49cc468d44ff1ab5621efcd
Dariia Sliusar <dariiasliusar@gmail.com>
2026-02-20 08:53:04 +0100

    Add Customer and Ticket models with migrations, and integrate Spatie roles in User model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Ticket extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+    protected $fillable = [
+        'customer_id',
+        'subject',
+        'message',
+        'status',
+        'replied_at',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
